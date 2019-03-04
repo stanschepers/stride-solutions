66265c7a20bf6f8e20e07bdd3b81eeabcb13a2bb
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2019-03-04 07:58:16 +0100

    Added bar plot for 1.2

--- plotStAn.py
+++ plotStAn.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import sys
 
 
@@ -33,7 +34,7 @@ def plot_cumulative_and_new_cases(file):
             outbreaks += 1
         data_points.plot(kind='line')
 
-    print("Numer of outbreaks: {nr}".format(nr=outbreaks))
+    # print("Numer of outbreaks: {nr}".format(nr=outbreaks))
 
     plt.xlabel('Day in Simulation')
     plt.ylabel('Total number of infected cases')
@@ -53,9 +54,24 @@ def plot_cumulative_and_new_cases(file):
     plt.close()  # clf
 
 
+def final_freq_hist(file):
+
+    data = pd.read_csv(file)
+
+    final_frequencies = data.iloc[[-1]].squeeze()
+
+    x = np.arange(len(final_frequencies))
+
+    plt.bar(x, height=final_frequencies)
+    plt.xticks([])
+    plt.show()
+    plt.close()
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Please supply (only) the name of the file containing stochastic "
               "analysis results.")
     else:
         plot_cumulative_and_new_cases(sys.argv[1])
+        final_freq_hist(sys.argv[1])
