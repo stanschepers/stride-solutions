c5c59268e33434eb3e75bf8b51722abaa2b6e9fd
Stan Schepers <stanpolschepers@gmail.com>
2019-03-05 10:49:31 +0100

    add profiling functions

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import csv
+import time
+import timeit
+
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib import pyplot as mp
 import os
 
@@ -49,38 +53,96 @@ def plotNewCases(outputPrefix, vaccinationLevels):
     plt.show()
 
 
-def runSimulation(outputPrefix, vaccinationLevel):
+def plotPorfiling(name, levels, timings):
+    """
+        Plot timings from profiling
+    """
+    plt.plot(levels, timings)
+    plt.xlabel(name)
+    plt.ylabel("time in secondes")
+    # plt.legend(legend)
+    plt.savefig('profiling_%s.png' % name)
+    plt.show()
+
+
+def runSimulation(outputPrefix=None, vaccinationLevel=None, numDay=None, populationSize=None, seedingRate=None,
+                  contactLogMode=None):
     # Set up simulator
     control = PyController(data_dir="data")
     # Load configuration from file
-    control.loadRunConfig(os.path.join("config", "outbreak_2019_estimates.xml"))
+    if populationSize:
+        control.loadRunConfig(os.path.join("config", "run_generate_default.xml"))
+    else:
+        control.loadRunConfig(os.path.join("config", "run_default.xml"))
     # Set some parameters
-    control.runConfig.setParameter("vaccine_rate", vaccinationLevel / 100)
+    if numDay:
+        control.runConfig.setParameter("num_days", numDay)
+
+    if vaccinationLevel:
+        control.runConfig.setParameter("vaccine_rate", vaccinationLevel / 100)
+
+    if populationSize:
+        control.runConfig.setParameter("geopop_gen.population_size", populationSize)
+
+    if seedingRate:
+        control.runConfig.setParameter("seeding_rate", seedingRate)
+
+    if contactLogMode:
+        control.runConfig.setParameter("contact_log_level", contactLogMode)
+
     control.runConfig.setParameter("output_cases", "false")
     control.runConfig.setParameter("contact_output_file", "false")
-    control.runConfig.setParameter("output_prefix", outputPrefix + "_" + str(vaccinationLevel))
-    control.runConfig.setParameter("seeding_rate", 0.00000334)  # Seed 2 infected persons in population of 600 000
+    control.runConfig.setParameter("output_prefix", "output/" + outputPrefix + "_" + str(int(time.time())))
     control.registerCallback(trackCases, EventType.Stepped)
     # Run simulation
     control.control()
 
 
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+
+    return wrapped
+
+
 def main():
     outputPrefix = "Demo"
     # vaccinationLevels = [60, 65, 66.1, 66.15, 66.2, 67, 70, 80]
-    vaccinationLevels = [61, 70, 78, 80]
+    vaccinationLevels = [66]
 
-    for nr in vaccinationLevels:
-        try:  # FIRST DELETE DIRECTORY FILES, BEFORE ADDING VALUES
-            os.remove(outputPrefix + '_' + str(nr) + '/cases.csv')
-        except OSError:
-            pass
+    days = np.linspace(start=10, stop=1000, num=20, dtype=np.int)
+    timings_days = []
+
+    populationSize = np.linspace(1000, 10 ** 7, num=1000, dtype=np.int)
+    timings_populationSize = []
+
+    imunnityRate = np.linspace(1, 100, endpoint=False, num=9)
+    timings_immunityRate = []
+
+    seedingRate = np.linspace(.00000166, .002, num=10)
+    timings_seedingRate = []
+
+    contactLogMode = ["None", "Transmissions"]
+    timings_contactLogMode = []
+
+    # for nr in vaccinationLevels:
+    try:  # FIRST DELETE DIRECTORY FILES, BEFORE ADDING VALUES
+        import shutil
+        shutil.rmtree("./output", ignore_errors=True)
+    except OSError:
+        pass
 
     # Run simulations
-    for v in vaccinationLevels:
-        runSimulation(outputPrefix, v)
+    print(len(days))
+    for i, d in enumerate(days):
+        print(i)
+        wrapped = wrapper(runSimulation, outputPrefix=outputPrefix, numDay=d)
+        timings_days.append(timeit.timeit(wrapped))
+
+    plotPorfiling("Days", days, timings_days)
+
     # Post-processing
-    plotNewCases(outputPrefix, vaccinationLevels)
+    # plotNewCases(outputPrefix, vaccinationLevels)
 
 
 if __name__ == "__main__":
