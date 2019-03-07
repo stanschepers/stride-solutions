b52a88649a1dddf27ddeb33f8bf6f559d3aac7dd
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2019-03-08 00:17:42 +0100

    More glorious plots

--- new_sim_and_plot.py
+++ new_sim_and_plot.py
@@ -105,15 +105,115 @@ def plotImmAvg(outputPrefix, vacRate=None, immRate=None, r0=None, seeds=None):
         plt.xlabel("Simulation day")
         plt.ylabel("New cases per day")
         plt.title(str(immRate) + "% immune " + str(len(seeds)) + " seed average")
-        plt.savefig('immunity_levels.png')
+        # plt.savefig('immunity_levels.png')
+        plt.show()
+
+
+def plotImmAvgAll(outputPrefix, vacRate=None, immRates=None, r0=None,
+                      seeds=None):
+    if seeds and immRates:
+
+        legend = []
+
+        for immRate in immRates:
+
+            results = []
+
+            for seed in seeds:
+                newCasesPerDay = []
+                prevCumulativeCases = 0
+
+                with open(os.path.join(outputPrefix + "_" + str(vacRate) + "_" +
+                                       str(immRate) + "_" + str(r0) + "_"
+                                       + str(seed), "cases.csv")) as csvfile:
+                    reader = csv.DictReader(csvfile)
+
+                    for row in reader:
+                        cumulativeCases = int(row["cases"])
+                        newCasesPerDay.append(cumulativeCases -
+                                              prevCumulativeCases)
+                        prevCumulativeCases = cumulativeCases
+
+                results.append(newCasesPerDay)
+
+            avg = []
+            days = range(len(results[0]))
+            sim_count = len(seeds)
+            for i in days:
+                avg_elem = 0
+                for list in results:
+                    avg_elem += list[i]
+                avg_elem /= sim_count
+                avg.append(avg_elem)
+
+            legend.append(str(immRate) + "% immune")
+            plt.plot(days, avg)
+
+        plt.xlabel("Simulation day")
+        plt.ylabel("New cases per day")
+        plt.title(str(len(seeds)) + " seed average")
+        plt.legend(legend)
+        # plt.savefig('immunity_levels.png')
+        plt.show()
+
+
+def plotImmAvgMultiR0(outputPrefix, vacRate=None, immRate=None, r0s=None,
+                      seeds=None):
+    if seeds and r0s:
+
+        legend = []
+
+        for r0 in r0s:
+
+            results = []
+
+            for seed in seeds:
+                newCasesPerDay = []
+                prevCumulativeCases = 0
+
+                with open(os.path.join(outputPrefix + "_" + str(vacRate) + "_" +
+                                       str(immRate) + "_" + str(r0) + "_"
+                                       + str(seed), "cases.csv")) as csvfile:
+                    reader = csv.DictReader(csvfile)
+
+                    for row in reader:
+                        cumulativeCases = int(row["cases"])
+                        newCasesPerDay.append(cumulativeCases -
+                                              prevCumulativeCases)
+                        prevCumulativeCases = cumulativeCases
+
+                results.append(newCasesPerDay)
+
+            avg = []
+            days = range(len(results[0]))
+            sim_count = len(seeds)
+            for i in days:
+                avg_elem = 0
+                for list in results:
+                    avg_elem += list[i]
+                avg_elem /= sim_count
+                avg.append(avg_elem)
+
+            legend.append("R0 = " + str(r0))
+            plt.plot(days, avg)
+
+        plt.xlabel("Simulation day")
+        plt.ylabel("New cases per day")
+        plt.title(
+            str(immRate) + "% immune " + str(len(seeds)) + " seed average")
+        plt.legend(legend)
+        # plt.savefig('immunity_levels.png')
         plt.show()
 
 
 if __name__ == "__main__":
     immunityLevels = [70.1, 70.2, 70.3, 70.4, 70.5, 70.6, 70.7, 70.8, 70.9]
-    rng_seeds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    rng_seeds = range(1, 11)
+    r0s = range(12, 19)
 
     for lvl in immunityLevels:
+    # for r0 in r0s:
         for seed in rng_seeds:
             runSim("AVG", immRate=lvl, rngSeed=seed)
-        plotImmAvg("AVG", immRate=lvl, seeds=rng_seeds)
+    # plotImmAvgMultiR0("AVG", immRate=70.8, r0s=r0s,  seeds=rng_seeds)
+    plotImmAvgAll("AVG", immRates=immunityLevels,  seeds=rng_seeds)
